27af96b754773b2304794320389c3f2fef95a0ee
Graziella Costa <100802546+grazie-lla@users.noreply.github.com>
2024-01-29 21:51:32 -0300

    adding implementation to the StudyController.java

--- src/main/java/br/com/ada/Main.java
+++ src/main/java/br/com/ada/Main.java
@@ -2,9 +2,11 @@ package br.com.ada;
 
 
 import br.com.ada.controller.PersonalController;
+import br.com.ada.controller.StudyController;
 import br.com.ada.controller.TasksController;
 import br.com.ada.domain.BaseTask;
 import br.com.ada.domain.PersonalTask;
+import br.com.ada.domain.StudyTask;
 import br.com.ada.repository.TasksRepository;
 import br.com.ada.service.TasksService;
 
@@ -17,6 +19,7 @@ public class Main {
         System.out.println ("Escolha o tipo de tarefa: ");
         System.out.println("1. Tarefas Pessoais");
         System.out.println("2. Tarefas Gerais");
+        System.out.println("3. Tarefas de Estudo");
 
         int choice = scanner.nextInt();
 
@@ -30,6 +33,11 @@ public class Main {
             TasksService<BaseTask> generalService = new TasksService<>(generalRepository);
             TasksController generalController = new TasksController(generalService);
             generalController.run();
+        } else if (choice == 3) {
+            TasksRepository<StudyTask> studyRepository = new TasksRepository<>();
+            TasksService<StudyTask> studyService = new TasksService<>(studyRepository);
+            StudyController studyController = new StudyController(studyService);
+            studyController.run();
         } else {
             System.out.println("Escolha inválida!");
         }
