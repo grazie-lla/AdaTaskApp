e97b1d9c9a65e6bfc180995a125efcfe9bf96648
Graziella Costa <100802546+grazie-lla@users.noreply.github.com>
2024-01-29 23:00:19 -0300

    refactored main

--- src/main/java/br/com/ada/Main.java
+++ src/main/java/br/com/ada/Main.java
@@ -4,9 +4,11 @@ package br.com.ada;
 import br.com.ada.controller.PersonalController;
 import br.com.ada.controller.StudyController;
 import br.com.ada.controller.TasksController;
+import br.com.ada.controller.WorkController;
 import br.com.ada.domain.BaseTask;
 import br.com.ada.domain.PersonalTask;
 import br.com.ada.domain.StudyTask;
+import br.com.ada.domain.WorkTask;
 import br.com.ada.repository.TasksRepository;
 import br.com.ada.service.TasksService;
 
@@ -16,30 +18,56 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println ("Escolha o tipo de tarefa: ");
-        System.out.println("1. Tarefas Pessoais");
-        System.out.println("2. Tarefas Gerais");
-        System.out.println("3. Tarefas de Estudo");
+        System.out.printf("Escolha o tipo de tarefa: " +
+                "%n1. Tarefas Gerais; " +
+                "%n2. Tarefas Pessoais; " +
+                "%n3. Tarefas de Estudo; " +
+                "%n4. Tarefas de Trabalho;" +
+                "%nInsira o número correspondente à opção escolhida:%n");
 
         int choice = scanner.nextInt();
 
-        if (choice == 1) {
-            TasksRepository<PersonalTask<String>> personalRepository = new TasksRepository<>();
-            TasksService<PersonalTask<String>> personalService = new TasksService<>(personalRepository);
-            PersonalController personalController = new PersonalController(personalService);
-            personalController.run();
-        } else if (choice == 2) {
-            TasksRepository<BaseTask> generalRepository = new TasksRepository<>();
-            TasksService<BaseTask> generalService = new TasksService<>(generalRepository);
-            TasksController generalController = new TasksController(generalService);
-            generalController.run();
-        } else if (choice == 3) {
-            TasksRepository<StudyTask> studyRepository = new TasksRepository<>();
-            TasksService<StudyTask> studyService = new TasksService<>(studyRepository);
-            StudyController studyController = new StudyController(studyService);
-            studyController.run();
-        } else {
-            System.out.println("Escolha inválida!");
+        switch (choice){
+            case 1:
+                runBaseTasks();
+                break;
+            case 2:
+                runPersonalTasks();
+                break;
+            case 3:
+                runStudyTasks();
+                break;
+            case 4:
+                runWorkTasks();
+                break;
+            default:
+                System.out.println("Escolha inválida!");
         }
     }
+    private static void runBaseTasks(){
+        TasksRepository<BaseTask> generalRepository = new TasksRepository<>();
+        TasksService<BaseTask> generalService = new TasksService<>(generalRepository);
+        TasksController generalController = new TasksController(generalService);
+        generalController.run();
+    }
+    private static void runPersonalTasks(){
+        TasksRepository<PersonalTask<String>> personalRepository = new TasksRepository<>();
+        TasksService<PersonalTask<String>> personalService = new TasksService<>(personalRepository);
+        PersonalController personalController = new PersonalController(personalService);
+        personalController.run();
+    }
+    public static void runStudyTasks(){
+        TasksRepository<StudyTask> studyRepository = new TasksRepository<>();
+        TasksService<StudyTask> studyService = new TasksService<>(studyRepository);
+        StudyController studyController = new StudyController(studyService);
+        studyController.run();
+    }
+
+    public static void runWorkTasks(){
+        TasksRepository<WorkTask> workRepository = new TasksRepository<>();
+        TasksService<WorkTask> workService = new TasksService<>(workRepository);
+        WorkController workController = new WorkController(workService);
+        workController.run();
+    }
+
 }
