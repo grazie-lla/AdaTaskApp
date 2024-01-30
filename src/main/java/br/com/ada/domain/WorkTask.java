229d1202a1d7d40c0f7de24095feb1a0079e8b93
Graziella Costa <100802546+grazie-lla@users.noreply.github.com>
2024-01-29 21:58:24 -0300

    adding new attributes to WorkTask

--- src/main/java/br/com/ada/domain/WorkTask.java
+++ src/main/java/br/com/ada/domain/WorkTask.java
@@ -1,17 +1,37 @@
 package br.com.ada.domain;
 
 public class WorkTask<T> extends BaseTask<T> {
-    public WorkTask(String title, String description, String deadline) {
+    private String project;
+    private String assignedTo;
+    public WorkTask(String title, String description, String deadline, String project, String assignedTo) {
         super(title, description, deadline);
+        this.project = project;
+        this.assignedTo = assignedTo;
+    }
+
+    public String getProject() {
+        return project;
+    }
+
+    public void setProject(String project) {
+        this.project = project;
+    }
+
+    public String getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void setAssignedTo(String assignedTo) {
+        this.assignedTo = assignedTo;
     }
 
     @Override
     public String toString() {
-        return "WorkTask{" +
-                "title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                ", deadline='" + deadline + '\'' +
-                ", id=" + id +
-                '}';
+        return "Título: " + title +
+                "\nDescrição: " + description +
+                "\nProjeto: "+ project +
+                "\nResponsável pela tarefa: "+ assignedTo +
+                "\nPrazo máximo: " + deadline +
+                "\nID: " + id;
     }
 }
